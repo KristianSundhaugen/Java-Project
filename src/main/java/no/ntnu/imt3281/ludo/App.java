b75c596ef05ba415e0d9e46289521f9b1e34fe76
Øivind Kolloen <okolloen@hig.no>
2016-06-27 10:27:57 +0200

    Initial commit with maven project setup

---
+++ src/main/java/no/ntnu/imt3281/ludo/App.java
@@ -0,0 +1,13 @@
+package no.ntnu.imt3281.ludo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
