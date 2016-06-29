51a84e1393941cbae2ffa264bc995b860f0f7c50
Øivind Kolloen <okolloen@hig.no>
2016-06-29 17:57:38 +0200

    Check for exact number of eyes on the dice to get to finish. When one player get all pieces to finish the game ends and the correct winner is declared.

--- src/test/java/no/ntnu/imt3281/ludo/logic/LudoTest.java
+++ src/test/java/no/ntnu/imt3281/ludo/logic/LudoTest.java
@@ -137,6 +137,7 @@ public class LudoTest {
 	 * the player does NOT get an extra turn if that six is used to move out of the home position.
 	 * 
 	 * When the user has moved (and did not get a six) turn goes to the next player.
+	 * Moving a piece returns true if legal move, false if piece could not be moved.
 	 */
 	@Test
 	public void needASixToGetStarted() {
@@ -155,7 +156,7 @@ public class LudoTest {
 		assertEquals(Ludo.BLUE, ludo.activePlayer(), 0);
 		ludo.throwDice(6);
 		assertEquals(Ludo.BLUE, ludo.activePlayer(), 0);
-		ludo.movePiece(Ludo.BLUE, 0, 1);	// Move players piece from start(0) to square 1
+		assertTrue(ludo.movePiece(Ludo.BLUE, 0, 1));	// Move players piece from start(0) to square 1
 		assertEquals(1, ludo.getPosition(Ludo.BLUE, 0));
 		
 		// Since player moved out of start, the next player should get his/her turn(s)
@@ -168,12 +169,119 @@ public class LudoTest {
 		
 		// It should now be the other players turn
 		assertEquals(Ludo.BLUE, ludo.activePlayer(), 0);
-		int dice = ludo.throwDice();					// Let server generate a throw
-		ludo.movePiece(Ludo.BLUE, 1, 1+dice);			// Move the piece
+		int dice = ludo.throwDice();						// Let server generate a throw
+		assertTrue(ludo.movePiece(Ludo.BLUE, 1, 1+dice));	// Move the piece
 		assertEquals(1+dice, ludo.getPosition(Ludo.BLUE, 0));
 		
-		// It should now be the other players turn
-		assertEquals(Ludo.RED, ludo.activePlayer(), 0);
-		
+		if (dice==6)	// If BLUE threw a six BLUE should keep playing 
+			assertEquals(Ludo.BLUE, ludo.activePlayer(), 0);
+		else			// If not it should be REDs turn
+			assertEquals(Ludo.RED, ludo.activePlayer(), 0);
 	}
+	
+	/**
+	 * Checks that player need exact number of eyes on the dice to move to finish.
+	 * When all players pieces is finished (at position 59) the game is completed and correct winner is set.
+	 * Also checks game status, "Created", "Initiated", "Started", "Finished".
+	 */
+	@Test
+	public void gameStates() {
+		Ludo ludo = new Ludo();
+		assertEquals("Created", ludo.getStatus());		// A game with no players are created
+		ludo.addPlayer("Player1");
+		assertEquals("Initiated", ludo.getStatus());	// A game with players are Initiated
+		ludo = new Ludo("Player1", "Player2", null, null);
+		assertEquals("Initiated", ludo.getStatus());
+
+		// Move first piece from start to finish (player RED)
+		ludo.throwDice(6);
+		assertEquals("Started", ludo.getStatus());		// A game where the dice has been thrown is started
+		assertTrue(ludo.movePiece(Ludo.RED, 0, 1));
+		
+		for (int move=0; move<11; move++) {
+			skipBlue(ludo);
+			ludo.throwDice(5);
+			assertTrue(ludo.movePiece(Ludo.RED, 1+move*5, 1+(move+1)*5));
+		}
+		assertEquals(56, ludo.getPosition(Ludo.RED, 0), 0);	// RED has 3 more to go before home
+		skipBlue(ludo);
+		assertEquals(Ludo.RED, ludo.activePlayer(), 0);		// Should be RED players turn
+		ludo.throwDice(5);									// RED can not move 5
+		assertEquals(Ludo.BLUE, ludo.activePlayer(), 0);	// Should be BLUE players turn
+		skipBlue(ludo);
+		ludo.throwDice(4);									// RED can not move 4
+		assertEquals(Ludo.BLUE, ludo.activePlayer(), 0);	// Should be BLUE players turn
+		skipBlue(ludo);
+		ludo.throwDice(3);									// RED CAN move 3
+		assertTrue(ludo.movePiece(Ludo.RED, 56, 59));		// This piece is now finished
+		assertEquals("Started", ludo.getStatus());
+		assertEquals(Ludo.BLUE, ludo.activePlayer(), 0);	// Should be BLUE players turn
+		skipBlue(ludo);
+		
+		// Move two more pieces
+		for (int piece=1; piece<3; piece++) {
+			ludo.throwDice(6);
+			assertEquals("Started", ludo.getStatus());
+			assertTrue(ludo.movePiece(Ludo.RED, 0, 1));
+			
+			for (int move=0; move<11; move++) {
+				skipBlue(ludo);
+				ludo.throwDice(5);
+				assertTrue(ludo.movePiece(Ludo.RED, 1+move*5, 1+(move+1)*5));
+			}
+			assertEquals(56, ludo.getPosition(Ludo.RED, piece), 0);	// RED has 3 more to go before home
+			skipBlue(ludo);
+			assertEquals(Ludo.RED, ludo.activePlayer(), 0);		// Should be RED players turn
+			ludo.throwDice(5);									// RED can not move 5
+			assertEquals(Ludo.BLUE, ludo.activePlayer(), 0);	// Should be BLUE players turn
+			skipBlue(ludo);
+			ludo.throwDice(4);									// RED can not move 4
+			assertEquals(Ludo.BLUE, ludo.activePlayer(), 0);	// Should be BLUE players turn
+			skipBlue(ludo);
+			ludo.throwDice(3);									// RED CAN move 3
+			assertTrue(ludo.movePiece(Ludo.RED, 56, 59));		// This piece is now finished
+			assertEquals("Started", ludo.getStatus());
+
+			assertEquals(Ludo.BLUE, ludo.activePlayer(), 0);	// Should be BLUE players turn
+			skipBlue(ludo);			
+		}
+		
+		// Red now has one more piece to move to finish
+		ludo.throwDice(6);
+		assertTrue(ludo.movePiece(Ludo.RED, 0, 1));
+		skipBlue(ludo);
+		// We will now try to throw several sixes in a row
+		assertEquals(6, ludo.throwDice(6), 0);
+		assertTrue(ludo.movePiece(Ludo.RED, 1, 7));
+		assertEquals(6, ludo.throwDice(6), 0);
+		assertTrue(ludo.movePiece(Ludo.RED, 7, 13));
+		assertEquals(6, ludo.throwDice(6), 0);
+		assertEquals(Ludo.BLUE, ludo.activePlayer(), 0);	// Should be BLUE players turn
+		skipBlue(ludo);
+		assertEquals(6, ludo.throwDice(6), 0);
+		assertTrue(ludo.movePiece(Ludo.RED, 13, 19));
+		assertEquals(6, ludo.throwDice(6), 0);
+		assertTrue(ludo.movePiece(Ludo.RED, 19, 25));
+		assertEquals(5, ludo.throwDice(5), 0);
+		assertEquals(Ludo.RED, ludo.activePlayer(), 0);		// Two sixes and then a five is ok
+		assertTrue(ludo.movePiece(Ludo.RED, 25, 30));
+		skipBlue(ludo);
+		for (int move=0; move<5; move++) {
+			assertEquals(5, ludo.throwDice(5), 0);
+			assertTrue(ludo.movePiece(Ludo.RED, 30+move*5, 35+move*5));
+			skipBlue(ludo);
+		}
+		// Last RED piece on position 55, need a four to get to finish
+		ludo.throwDice(4);
+		assertTrue(ludo.movePiece(Ludo.RED, 55, 59));
+		assertEquals("Finished", ludo.getStatus());			// A game with a winner is finished
+		assertEquals(Ludo.RED, ludo.getWinner(), 0);
+	}
+
+	private void skipBlue(Ludo ludo) {
+		for (int noBlue=0; noBlue<3; noBlue++) {			// We will be moving the red players pieces only
+			ludo.throwDice(1);								// So blue only throws ones
+		}
+	}
+
 }
