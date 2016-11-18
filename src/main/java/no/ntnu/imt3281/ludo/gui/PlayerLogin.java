2097890054585a019ba5c9b45b3fb7208f8df331
simendahlum <mulhad_nemis@hotmail.com>
2016-11-18 15:17:55 +0100

    login scene added and code in plyerlogin

--- src/main/java/no/ntnu/imt3281/ludo/gui/PlayerLogin.java
+++ src/main/java/no/ntnu/imt3281/ludo/gui/PlayerLogin.java
@@ -1,22 +1,68 @@
 package no.ntnu.imt3281.ludo.gui;
 
+import java.awt.event.ActionEvent;
+import java.io.IOException;
+
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPasswordField;
 import javax.swing.JTextField;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+/**
+ * 
+ * @author Simen
+ * This class will handle login
+ * Check password and username
+ */
 public class PlayerLogin extends JFrame {
 	
-	JButton loginButton;
+	@FXML
+	private JButton loginButton;
 	
-	JLabel user;
-	JLabel pass;
+	@FXML
+	private JLabel user;
 	
-	JTextField username;
-	JPasswordField password;
+	@FXML
+	private JLabel pass;
+	
+	@FXML
+	private JTextField username;
+	
+	@FXML
+	private JPasswordField password;
+	
+	@FXML
+	private JLabel errorMessage;
 	
 	public PlayerLogin(){
+		
+	}
+	
+	/**
+	 * Handles login button press
+	 */
+	@FXML
+	public void handleLoginButton(ActionEvent event) {
+		
+		if(username.getText().equals("admin") && password.getPassword().equals("admin")){
+			try {
+				Parent parent = FXMLLoader.load(getClass().getResource("Login.fxml"));
+				Stage stage = new Stage();
+				Scene scene = new Scene(parent);
+				stage.setScene(scene);
+				stage.show();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}else {
+			errorMessage.setText("Username or password is invalid");			
+		}
 		
 	}
 }
