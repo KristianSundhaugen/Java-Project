d9c1c38505a7098f9c31a3ee4cb464112296b8c2
Lasse Sviland <lassesviland@hotmail.com>
2016-11-23 16:11:20 +0100

    removed old unused clode from main

--- src/main/java/no/ntnu/imt3281/ludo/client/Client.java
+++ src/main/java/no/ntnu/imt3281/ludo/client/Client.java
@@ -51,21 +51,5 @@ public class Client extends Application {
 
 	public static void main(String[] args) {
 		launch(args);
-	/**	 String serverAddress = "localhost";
-		        Socket socket = new Socket(serverAddress, 9090);
-		        PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
-		        out.println(new Message("chat message 1","CHAT", "1").toString());
-		        out.println(new Message("game message 1","GAME", "4").toString());
-		        out.println(new Message("chat message 2","CHAT", "2").toString());
-		        out.println(new Message("game message 2","GAME", "4").toString());
-		        out.println(new Message("chat message 3","CHAT", "1").toString());
-		        out.println(new Message("game message 3","GAME", "4").toString());
-		        out.println(new Message("chat message 4","CHAT", "4").toString());
-		        out.println(new Message("game message 4","GAME", "4").toString());
-	
-		        BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-		        String answer = input.readLine();
-		        JOptionPane.showMessageDialog(null, answer);
-		        System.exit(0);*/
 	}
 }
