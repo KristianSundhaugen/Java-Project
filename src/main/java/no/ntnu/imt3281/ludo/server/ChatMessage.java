f84544552d96992ff6c6c55b291c644a64d51bc1
Lasse Sviland <lassesviland@hotmail.com>
2016-11-23 16:05:10 +0100

    added function to parse a chat message and return chat information

---
+++ src/main/java/no/ntnu/imt3281/ludo/server/ChatMessage.java
@@ -0,0 +1,40 @@
+package no.ntnu.imt3281.ludo.server;
+
+/**
+ * 
+ * @author Lasse Sviland
+ *
+ */
+public class ChatMessage {
+	private Message msg;
+	private String username;
+	private String messageContent;
+	public ChatMessage(Message msg) {
+		this.msg = msg;
+		String[] messageParts = msg.getMessage().split(":");
+		this.username = messageParts[0];
+		this.messageContent = messageParts[1];
+	}
+
+	/** 
+	 * @return the id of the game sent from the server
+	 */
+	public String getUsername() {
+		return username;
+	}	
+	
+	/** 
+	 * @return the messageContent
+	 */
+	public String getMessageContent() {
+		return messageContent;
+	}
+	
+	/** 
+	 * @return the client that sent the message
+	 */
+	public ServerClient getClient() {
+		return msg.getClient();
+	}
+	
+}
