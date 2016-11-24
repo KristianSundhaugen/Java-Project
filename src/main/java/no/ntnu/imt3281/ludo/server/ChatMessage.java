da7c9b8533e323ff0bf52703b68e07834c3371a8
Lasse Sviland <lassesviland@hotmail.com>
2016-11-24 03:43:40 +0100

    added functions for lists, made chat message extend message

--- src/main/java/no/ntnu/imt3281/ludo/server/ChatMessage.java
+++ src/main/java/no/ntnu/imt3281/ludo/server/ChatMessage.java
@@ -5,15 +5,16 @@ package no.ntnu.imt3281.ludo.server;
  * @author Lasse Sviland
  *
  */
-public class ChatMessage {
-	private Message msg;
+public class ChatMessage extends Message {
 	private String username;
 	private String messageContent;
 	public ChatMessage(Message msg) {
-		this.msg = msg;
-		String[] messageParts = msg.getMessage().split(":");
-		this.username = messageParts[0];
-		this.messageContent = messageParts[1];
+		super(msg);
+		this.username = stringPart(0);
+		try {
+			this.messageContent = stringPart(1);
+		} catch (Exception e) {}
+		
 	}
 
 	/** 
@@ -30,11 +31,25 @@ public class ChatMessage {
 		return messageContent;
 	}
 	
-	/** 
-	 * @return the client that sent the message
+	/**
+	 * @return if it is a request for getting a list of chats
 	 */
-	public ServerClient getClient() {
-		return msg.getClient();
+	public boolean isListRequest() {
+		return (getId().equals("-1") && getMessage().equals("LIST"));
+	}
+	
+	/**
+	 * @return if it is a response with possible chats to join
+	 */
+	public boolean isListResponse() {
+		return (stringPart(0).equals("LIST_ROOMS_RESPONSE"));
+	}
+
+	/**
+	 * @return if it is a request to join a new chat
+	 */
+	public boolean isNewChat() {
+		return (stringPart(0).equals("NEW_CHAT_JOIN"));
 	}
 	
 }
